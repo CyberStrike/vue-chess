cfbc7ac737be5870e6c23e7bab33f8e0d252bc7f
Chris Scott <cyberstrike@gmail.com>
2023-04-11 18:31:25 -0700

    🎉 Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'standard-with-typescript',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['vue', 'import'],
+  ignorePatterns: ['dist', 'node_modules', '*.d.ts'],
+  rules: {
+    'import/no-unresolved': 'error',
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+    'import/parsers': '@typescript-eslint/parser',
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  }
+}
